f2097d07abd2f6397d699f5e31d87d2579099ff7
LuizCD Prof <luizcdprof@gmail.com>
2024-04-11 11:32:36 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "LuizCD Prof"
+git config --global user.email "luizcdprof@gmail.com"
+
+cd C:\Users\JamilyNarcizoDosSant\
+
+git clone https://github.com/LuizCD/Caesar-Cipher.git
+cd Caesar-Cipher
+
+code .
