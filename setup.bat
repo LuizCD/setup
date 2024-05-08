9bdb491dc721f01eead060ce3fe4c30893f8ebd5
LuizCD Prof <luizcdprof@gmail.com>
2024-05-07 23:24:59 -0300

    Updates batch file

--- setup.bat
+++ setup.bat
@@ -3,7 +3,6 @@ git config --global user.name "LuizCD Prof"
 git config --global user.email "luizcdprof@gmail.com"
 
 REM Ir para a pasta do usuário
-REM cd C:\Users\JamilyNarcizoDosSant\
 cd %userprofile%
 
 REM Fazer o clone do repositório do projeto do Github
@@ -12,8 +11,5 @@ git clone https://github.com/LuizCD/Caesar-Cipher.git
 REM Entrar na pasta do projeto
 cd Caesar-Cipher
 
-REM Criar o ambiente virtual
-python -m venv venv
-
-REM Abrir o VSCODE
+REM Abrir o VS Code na pasta do projeto
 code .
